47daa5c98d4b0bba98e008c56e387ca300241403
Vanessa Le <vcle@dons.usfca.edu>
2025-02-13 16:15:46 -0800

    Link fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 // App.jsx
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -17,11 +12,10 @@ const App = () => {
     <Router basename="/personal-portfolio">
       <div className="min-h-screen bg-color-2">
         <Routes>
-          <Route path="/" element={<Navigate to="/personal-portfolio" />} />
-          <Route path="/personal-portfolio" element={<Home />} />
-          <Route path="/personal-portfolio/about" element={<About />} />
-          <Route path="/personal-portfolio/projects" element={<Projects />} />
-          <Route path="/personal-portfolio/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
